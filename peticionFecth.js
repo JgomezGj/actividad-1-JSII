12032dab746611221c38351af7611b2d58f057d8
Jesus Maria Gomez Jimenez <jesussmgomez@gmail.com>
2024-01-23 15:20:21 -0500

    Segunda actualización: hasta ontener nombre y tipo

--- peticionFecth.js
+++ peticionFecth.js
@@ -10,7 +10,8 @@ try {
 
     const dato = await respuesta.json();
 
-    return dato;
+    return (dato);
+    
 } catch (error) {
     console.error(error);
 }
@@ -42,8 +43,41 @@ async function obtenerDatosPorTipo(nombrePokemon) {
     }
 }
 
+async function obtenerNombreYTipo(nombrePokemon){
+    try {
+        let datosPokemon = await obtenerDatosPokemon(nombrePokemon);
+
+        if (datosPokemon.types && datosPokemon.types.length > 0) {
+            const tipos = datosPokemon.types.map((tipo) => tipo.type.name);
+            console.log(`Nombre pokemon: ${nombrePokemon} Tipo: ${tipos}`);
+        } else {
+            console.log(`El Pokémon ${nombrePokemon} no tiene información de tipo.`);
+            return {nombre: datosPokemon.name, tipos: [] };
+        }
+    } catch (error) {
+        console.error("Error al obtener el nombre y tipo del Pokemon", error);
+        return null;
+    }
+}
+
+async function obtenerListaPokemones() {
+    try {
+        const listaPokemonPro = Array.from({length: 50}, async (_, index) => {
+            let numPokemon = index + 1;
+            let nombrePokemon = numPokemon.toString();
+            let datosPokemon = await obtenerDatosPokemon(nombrePokemon);
+            return datosPokemon.name;
+        });
+
+        const listaPokemon = await Promise.all(listaPokemonPro);
+        console.log("Lista de los primeros 50 Pokémon: ", listaPokemon);
+    }catch (error) {
+        console.error("Error al obtener la lista de pokemones", error)
+    }
+}
+
 async function obtenerDatosPorNombre() {
-    let nombrePokemon = "pikachu";
+    let nombrePokemon = "ivysaur";
 
     try {
         let datosPokemon = await obtenerDatosPokemon(nombrePokemon);
@@ -51,6 +85,10 @@ async function obtenerDatosPorNombre() {
 
         await obtenerHabilidades(nombrePokemon);
         await obtenerDatosPorTipo(nombrePokemon);
+        await obtenerListaPokemones();
+        await obtenerNombreYTipo(nombrePokemon);
+        
+    
 
     } catch (error) {
         console.error("Error al obtener datos del pokemon");
